9a62ee0f8d34a82828e3ec26e819ac7ba69cc84f
SaumyaSingh1 <ssaumyas100@gmail.com>
2021-01-17 20:22:00 +0530

    Most Important Topic of Open Source

--- bubbleSort.c
+++ bubbleSort.c
@@ -20,6 +20,10 @@ fourth pass: {1,2,3,7,12,56}
 			 {1,2,3,7,12,56}
 Sorted array: {1,2,3,7,12,56}*/
 
+
+Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
+
+
 #include<stdio.h>
 
 void swap(int *x, int *y)
@@ -40,3 +44,13 @@ void display(int arr[], int size)
 	}
 	printf("\n");
 }
+
+
+
+Worst and Average Case Time Complexity: O(n*n). Worst case occurs when array is reverse sorted.
+
+Best Case Time Complexity: O(n). Best case occurs when array is already sorted.
+
+Auxiliary Space: O(1)
+
+Boundary Cases: Bubble sort takes minimum time (Order of n) when elements are already sorted.
